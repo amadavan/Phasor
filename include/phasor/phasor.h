684b43a81a1974dd94fca26174dde93ca6037ac7
Avinash Madavan <avinash.madavan@gmail.com>
2021-01-11 17:57:59 -0800

    Add root header

---
+++ include/phasor/phasor.h
@@ -0,0 +1,13 @@
+#ifndef PHASOR_PHASOR_H_
+#define PHASOR_PHASOR_H_
+
+#include "matpower/branch.h"
+#include "matpower/bus.h"
+#include "matpower/gen.h"
+#include "matpower/gencost.h"
+#include "matpower/matpower_dataset.h"
+#include "matpower/matpower_index.h"
+#include "matpower/matpower_parser.h"
+#include "matpower/network.h"
+
+#endif // PHASOR_PHASOR_H_
